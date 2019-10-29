e36c2f12408d0c6015996e137e6c0a840afb13ac
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2019-10-30 08:54:05 +1100

    Add install_bash script.

---
+++ test/install_bash.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash -ex
+
+if [ -z $INSTALL_BASH_VERSION ]; then
+    echo "No \$INSTALL_BASH_VERSION, using default Bash"
+    exit
+fi
+
+set -u
+
+echo "Installing bash $INSTALL_BASH_VERSION..."
+
+mkdir -p bash
+cd bash
+wget -N https://ftpmirror.gnu.org/bash/bash-$INSTALL_BASH_VERSION.tar.gz
+tar xvf bash-$INSTALL_BASH_VERSION.tar.gz
+pushd bash-$INSTALL_BASH_VERSION
+
+patches="../bash-$INSTALL_BASH_VERSION-patches/bash$(echo $INSTALL_BASH_VERSION | tr -d .)-???"
+for patch in $(find .. -wholename "$patches" | sort); do
+    echo "Applying $patch"
+    patch -f -p0 <$patch
+done
+
+./configure --exec-prefix /
+make
+sudo make install
+
+popd
+rm -rf bash-$INSTALL_BASH_VERSION
